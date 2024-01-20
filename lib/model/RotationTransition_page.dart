0ec2262d303901a4c03d59566b167ae3974a452e
fab-raul <153070947+fab-raul@users.noreply.github.com>
2024-01-20 12:47:39 +0100

    Update RotationTransition_page.dart
    rotation transition updated

--- lib/model/RotationTransition_page.dart
+++ lib/model/RotationTransition_page.dart
@@ -1,20 +1,92 @@
 import 'package:flutter/material.dart';
 
-class RotationAnime extends StatelessWidget {
-  const RotationAnime({super.key});
+void main() => runApp(const RotationTransitionExampleApp());
+
+class RotationTransitionExampleApp extends StatelessWidget {
+  const RotationTransitionExampleApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: RotationTransitionExample(),
+    );
+  }
+}
+
+class RotationTransitionExample extends StatefulWidget {
+  const RotationTransitionExample({Key? key}) : super(key: key);
+
+  @override
+  State<RotationTransitionExample> createState() =>
+      _RotationTransitionExampleState();
+}
+
+class _RotationTransitionExampleState extends State<RotationTransitionExample>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 2),
+    vsync: this,
+  )..repeat(reverse: true);
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.elasticOut,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _startAnimation() {
+    _controller.repeat(reverse: true); // <-- Start repeating here
+  }
+
+  void _stopAnimation() {
+    if (_controller.isAnimating) {
+      _controller.stop();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       appBar: AppBar(
-        title: Text(
-          'RotationTransition animation',
+        title: const Text(
+          'Illustration of Rotation Transition Animation',
           style: TextStyle(
-            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
           ),
         ),
-        backgroundColor: Colors.blue,
+        centerTitle: true,
       ),
+      body: Center(
+        child: RotationTransition(
+          turns: _animation,
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: FlutterLogo(size: 150.0),
+          ),
+        ),
+      ),
+      persistentFooterButtons: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _startAnimation,
+              child: const Text('Start'),
+            ),
+            const SizedBox(width: 8),
+            ElevatedButton(
+              onPressed: _stopAnimation,
+              child: const Text('Stop'),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
